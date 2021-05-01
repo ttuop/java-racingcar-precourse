de0542d1ab2f1bb11fcbdb33ed22429209c42d2e
ttuop <plintcsz@gmail.com>
2021-05-01 16:23:08 +0900

    feat: Engine.run() 유효성 체크 추가

--- src/main/java/game/racingcar/Engine.java
+++ src/main/java/game/racingcar/Engine.java
@@ -3,6 +3,10 @@ package game.racingcar;
 public class Engine {
 
 	public MoveResult run(Fuel fuel) {
+		if (fuel == null) {
+			return MoveResult.STOP;
+		}
+
 		return fuel.isEnough() ? MoveResult.GO : MoveResult.STOP;
 	}
 
