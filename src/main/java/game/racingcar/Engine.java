eca9d72c460fa1eb49d15732c40ecdae7d3aa3b0
ttuop <plintcsz@gmail.com>
2021-05-01 18:43:34 +0900

    test: Engine 테스트 케이스 추가
    * Fuel 객체가 null 인 경우 추가

--- src/main/java/game/racingcar/Engine.java
+++ src/main/java/game/racingcar/Engine.java
@@ -1,10 +1,12 @@
 package game.racingcar;
 
+import static game.racingcar.Fuels.*;
+
 public class Engine {
 
 	public MoveResult run(Fuel fuel) {
 		if (fuel == null) {
-			return MoveResult.STOP;
+			throw new IllegalArgumentException(INVALID_FUEL_MESSAGE);
 		}
 
 		return fuel.isEnough() ? MoveResult.GO : MoveResult.STOP;
--- src/test/java/game/racingcar/EngineTest.java
+++ src/test/java/game/racingcar/EngineTest.java
@@ -1,7 +1,10 @@
 package game.racingcar;
 
+import static game.racingcar.Fuels.*;
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -9,16 +12,26 @@ public class EngineTest {
 
 	Engine engine = new Engine();
 
+	@DisplayName("엔진이 정지하는 경우 테스트")
 	@ParameterizedTest
 	@ValueSource(ints = {0, 3})
 	void testStop(int input) {
 		assertThat(engine.run(new Fuel(input))).isEqualTo(MoveResult.STOP);
 	}
 
+	@DisplayName("엔진이 이동하는 경우 테스트")
 	@ParameterizedTest
 	@ValueSource(ints = {4, 9})
 	void testMove(int input) {
 		assertThat(engine.run(new Fuel(input))).isEqualTo(MoveResult.GO);
+	}
+
+	@DisplayName("연료가 null 인 경우 테스트")
+	@Test
+	void testNullFuel() {
+		assertThatThrownBy(() -> engine.run(null))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage(INVALID_FUEL_MESSAGE);
 	}
 
 }
